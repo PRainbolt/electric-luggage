b43ba2a931b19b318036e5e21d5cbe454b2b7932
PRainbolt <phillip.rainbolt@gmail.com>
2019-11-21 19:32:32 -0600

    1st Commit of Basic Android Application Code

---
+++ app/src/main/java/com/example/demoapplication/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.demoapplication
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.util.Log.d
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        EnterButton.setOnClickListener {
+            d( "Phillip", "button was pressed!")
+            startActivity(Intent(this, AboutMe::class.java))
+        }
+
+    }
+}
